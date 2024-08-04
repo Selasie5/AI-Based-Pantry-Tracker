856ff91c4a9570bf8c229e5ed66e1bb504f399fc
Selasie5 <selasisepenu5@gmail.com>
2024-08-04 09:21:34 +0000

    Finalize google auth integrtion

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -79,7 +79,7 @@ export default function SignUp() {
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
+        const token = credential?.accessToken;
         // The signed-in user info.
         const user = result.user;
         console.log("User signed in: ", user);
